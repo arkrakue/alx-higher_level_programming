3fc74584164568cb3515986ef3f8d610d5006538
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-05-11 17:05:14 +0000

    Python Imports and Modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
-argv = sys.argv[1:]
-arg_num = len(argv)
-if arg_num == 1:
-    print(f"{arg_num} argument:")
-elif arg_num == 0:
-    print(f"{arg_num} arguments.")
-else:
-    print(f"{arg_num} arguments:")
-for i, arg in enumerate(argv):
-    print('{}: {}'.format(i + 1, arg))
+if __name__ == "__main__":
+    argv = sys.argv[1:]
+    arg_num = len(argv)
+    if arg_num == 1:
+        print(f"{arg_num} argument:")
+    elif arg_num == 0:
+        print(f"{arg_num} arguments.")
+    else:
+        print(f"{arg_num} arguments:")
+        for i, arg in enumerate(argv):
+            print('{}: {}'.format(i + 1, arg))
