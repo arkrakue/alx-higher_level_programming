0dd6de83a60b513586959d6d736e3b73c7957796
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-05-11 16:50:28 +0000

    Python Imports and Modules

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     sub_results = sub(a, b)
     mul_results = mul(a, b)
     div_results = div(a, b)
-    print("{} +{} = {}".format(a, b, add_results))
+    print("{} + {} = {}".format(a, b, add_results))
     print("{} - {} = {}".format(a, b, sub_results))
     print("{} * {} = {}".format(a, b, mul_results))
     print("{} / {} = {}".format(a, b, div_results))
