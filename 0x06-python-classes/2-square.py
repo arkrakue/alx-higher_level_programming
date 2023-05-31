b08a87e2d366d4f2a1ac0e97eb968ce57f6ae4f9
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-05-31 01:14:06 +0000

    Square Class with size raises

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-"""Square Class"""
+"""Square class"""
 
 
 class Square:
     """Square class with its size"""
 
     def __init__(self, size=0):
-        """ Square Object
+        """Square object
 
         Args:
-            size (int): The size of the square
+            size (int): The size of the new square
 
         Raises:
             TypeError: If size is not an integer
             ValueError: If size is less than 0
         """
-
-    if not isinstance(size, int):
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-
-    self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
