53fa1be3ff7ba9e558c9eb8ccad22c5845d90d62
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-05-31 00:16:00 +0000

    Empty Square Class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """Square Class"""
 
+
 class Square:
     """Square Class representation"""
     def __init__(self):
         """Square Object"""
         pass
+
+
 if __name__ == "__main__":
     """Test Square Object"""
     square = Square()
