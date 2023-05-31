208f498fa8e5efebf14f8d54a87e91f126462a8c
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-05-31 02:05:50 +0000

    Singly linked list

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,39 +1,68 @@
 #!/usr/bin/python3
+"""
+This module defines the Node and SinglyLinkedList classes.
+"""
 
 
 class Node:
-
+    """
+    This class represents a node of a singly linked list.
+    """
     def __init__(self, data, next_node=None):
+        """
+        Initializes a Node object with data and the next node reference.
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
+        """
+        Retrieves the data of the node.
+        """
         return self.__data
 
     @data.setter
     def data(self, value):
+        """
+        Sets the data of the node.
+        """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """
+        Retrieves the reference to the next node.
+        """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
+        """
+        Sets the reference to the next node.
+        """
         if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
-
+    """
+    This class represents a singly linked list.
+    """
     def __init__(self):
+        """
+        Initializes an empty singly linked list.
+        """
         self.head = None
 
     def sorted_insert(self, value):
+        """
+        Inserts a new Node into the correct sorted position in the list
+        (increasing order).
+        """
         new_node = Node(value)
         if self.head is None:
             self.head = new_node
@@ -49,6 +78,9 @@ class SinglyLinkedList:
             current.next_node = new_node
 
     def __str__(self):
+        """
+        Returns a string representation of the singly linked list.
+        """
         result = ""
         current = self.head
         while current is not None:
@@ -58,9 +90,12 @@ class SinglyLinkedList:
 
 
 if __name__ == "__main__":
+    """
+    Create and test SinglyLinkedList objects.
+    """
     linked_list = SinglyLinkedList()
     linked_list.sorted_insert(3)
     linked_list.sorted_insert(1)
     linked_list.sorted_insert(2)
     linked_list.sorted_insert(5)
-    print(linked_list)
+    print(linked_list)  # Output: 1\n2\n3\n5
