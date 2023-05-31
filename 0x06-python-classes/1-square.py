a0c849c253e1b41137a6a2715b3a87d92bc1aa6d
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-05-31 00:40:41 +0000

    Square class with size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Square class"""
+
+
+class Square:
+    """Square class with its size"""
+
+    def __init__(self, size):
+        """ Square object
+
+        Args:
+            size (int): The size of the square
+        """
+        self.__size = size
