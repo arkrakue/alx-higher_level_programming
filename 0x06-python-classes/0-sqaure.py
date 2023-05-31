3d4788994c03069fa572e987713b27965ed4709c
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-05-31 00:06:12 +0000

    Empty Square Class

---
+++ 0x06-python-classes/0-sqaure.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Square Class"""
+
+class square:
+    def __int__(self):
+        """Square Object"""
+        pass
+if __name__ == "__main__":
+    """Test Square Object"""
+    square = square()
