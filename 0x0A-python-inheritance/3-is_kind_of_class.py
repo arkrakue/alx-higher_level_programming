69f39f00e3c136545f3d3c2586fc7d4461514015
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-06-13 00:58:04 +0000

    Task 3 == Isinstance Function, Task 2 == Extact object

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -9,11 +9,11 @@ def is_kind_of_class(obj, a_class):
     Returns True if the object is an instance of, or if the object
     is an instance of a class that inherited from,
     the specified class; otherwise False.
-    
+
     Args:
         obj: The object to check.
         a_class: The class to check against.
-        
+
     Returns:
         A boolean indicating whether the object is an instance of,
         or if the object is an instance of a class that inherited from,
