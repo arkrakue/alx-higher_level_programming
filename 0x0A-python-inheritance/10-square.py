a32ab370ce32f5ed410db45c16ee4a5caffa885d
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-06-13 02:21:46 +0000

    square

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Creates class Square that inherits Rectangle
+and takes argument: size.
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Class Square that inherits Rectangle and takes argument: size.
+    """
+    def __init__(self, size):
+        """
+        Initializes a new Square object with the given size.
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+
+    def area(self):
+        """
+        Returns the area of the square.
+        """
+        return self.__size ** 2
+
+    def __str__(self):
+        """
+        Returns a string representation of the square.
+        """
+        return "[Square] {}/{}".format(self.__size, self.__size)
