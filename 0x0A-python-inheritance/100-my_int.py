9adeb3f21941ea97bfb0a38f6076c72f6ee9911b
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-06-13 02:41:58 +0000

    My integer

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,23 +1,23 @@
 ##!/usr/bin/python3
-'''
+"""
     Creates a class MyInt that inherits from int
-'''
+"""
 
 
 class MyInt(int):
-    '''
+    """
         Class MyInt that inherits from int,
         has the == and != operators inverted
-    '''
+    """
 
     def __eq__(self, other):
-        '''
+        """
             Return boolean if the self and other are equal
-        '''
+        """
         return super().__ne__(other)
 
     def __ne__(self, other):
-        '''
+        """
             Return boolean if the self and other are equal
-        '''
+        """
         return super().__eq__(other)
