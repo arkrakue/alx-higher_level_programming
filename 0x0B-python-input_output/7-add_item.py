4fac138d1ac94d2e7c407400081f6a8ece7e858a
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-06-14 02:06:18 +0000

    Load, add and save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Adds arguments to the pytho list
+"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+filename = "add_item.json"
+my_list = []
+
+if path.exists(filename):
+    my_list = load_from_json_file(filename)
+
+for arg in sys.argv[1:]:
+    my_list.append(arg)
+
+save_to_json_file(my_list, filename)
