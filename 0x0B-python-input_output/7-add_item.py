5b1a504a02aa68d012bb12c68ab513db733bb694
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-06-14 02:17:46 +0000

    Load, add and save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,38 +3,19 @@
 Adds arguments to the pytho list
 """
 import sys
-from os import path
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
 
-def save_to_json_file(my_obj, filename):
-    """
-    Writes an object to a text file, using a JSON representation.
-    :param my_obj: The object to serialize.
-    :param filename: The name of the file to write to.
-    """
-    import json
-    with open(filename, 'w', encoding='utf-8') as f:
-        json.dump(my_obj, f)
+try:
+    new_list = load_from_json_file('add_item.json')
+except:
+    new_list = []
 
+for i in range(1, len(sys.argv)):
+    new_list.append(sys.argv[i])
 
-def load_from_json_file(filename):
-    """
-    Creates an object from a "JSON file".
-    :param filename: The name of the file to read from.
-    :return: The deserialized object.
-    """
-    import json
-    with open(filename, 'r', encoding='utf-8') as f:
-        return json.load(f)
-
-
-filename = "add_item.json"
-my_list = []
-
-if path.exists(filename):
-    my_list = load_from_json_file(filename)
-
-for arg in sys.argv[1:]:
-    my_list.append(arg)
-
-save_to_json_file(my_list, filename)
+try:
+    save_to_json_file(new_list, 'add_item.json')
+except:
+    pass
