a0c30ae384d7324d5ec545dcabd5a61847ca7085
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-06-14 02:16:08 +0000

    Load, add and save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,8 +4,28 @@ Adds arguments to the pytho list
 """
 import sys
 from os import path
-from '5-save_to_json_file' import save_to_json_file
-from'6-load_from_json_file' import load_from_json_file
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Writes an object to a text file, using a JSON representation.
+    :param my_obj: The object to serialize.
+    :param filename: The name of the file to write to.
+    """
+    import json
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
+
+
+def load_from_json_file(filename):
+    """
+    Creates an object from a "JSON file".
+    :param filename: The name of the file to read from.
+    :return: The deserialized object.
+    """
+    import json
+    with open(filename, 'r', encoding='utf-8') as f:
+        return json.load(f)
 
 
 filename = "add_item.json"
