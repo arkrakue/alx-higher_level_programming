6f2d738be432cbf94114e28681214ef4b1045696
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-06-14 01:22:27 +0000

    from JSON

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Returns a python object represented
+by a JSON string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Convert a json string to a python object
+    Arguments:
+        my_obj (str): The inputed object to convert in json format
+    Return:
+        A Python object from json
+    """
+    return json.loads(my_str)
