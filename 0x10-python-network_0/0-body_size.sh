7c5796eef237863865c7122ba5673f746d1f55a1
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-09-02 01:11:21 +0000

    BODY SIZE  new

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 # The script sends a request to URL and displays the size of the body of the response
-curl -sI '$1' | grep -i Content-Length | awk "{print $2}"
+curl -sI "$1" | wc -c | xargs | sed 's/$/ /'
