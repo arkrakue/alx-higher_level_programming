b21622d58f818c65749d2b95de87aad9cc3f25f0
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-09-27 13:56:52 +0000

    Write me

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+var fs = require('fs');
+var filePath = process.argv[2];
+var content = process.argv[3];
+
+fs.writeFile(filePath, content, 'utf8', function(err) {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('File written successfully');
+});
