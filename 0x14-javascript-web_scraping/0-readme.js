cbb2a41e197b69060743d509139d5da9fe47d874
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-09-27 13:50:31 +0000

    Readme update

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-# Script reads and prints the content of a file
-
 const fs = require('fs');
-const file = process.argv[2];
+const filepath= process.argv[2];
 
-fs.readFile(file, 'utf8', (err, data) => {
+fs.readFile(filepath, 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
