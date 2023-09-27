0e896076240b12eac33f8bec0bfcbf98c695093d
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-09-27 14:03:45 +0000

    Write me Update

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
-var fs = require('fs');
-var filePath = process.argv[2];
-var content = process.argv[3];
+const fs = require('fs');
+const filePath = process.argv[2];
+const content = process.argv[3];
 
 fs.writeFile(filePath, content, 'utf8', function(err) {
     if (err) {
         console.error(err);
+	    return;
     }
 });
