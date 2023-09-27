1b6180d961e1d1da290a7710e8849d2e9290f872
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-09-27 14:41:12 +0000

    Right order update

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,28 +1,29 @@
 #!/usr/bin/node
 const request = require('request');
 const movieId = process.argv[2];
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-request(apiUrl, function (error, response, body) {
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+request(url, function (error, response, body) {
   if (error) {
-    console.error('An error occurred:', error);
-    return;
+    console.log(error);
+  } else {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
-  const film = JSON.parse(body);
-  const characters = film.characters;
-  const count = characters.length;
-  characters.forEach(function(characterUrl, index) {
-    request(characterUrl, function (error, response, body) {
-      if (error) {
-        console.error('An error occurred:', error);
-        return;
-      }
-      const character = JSON.parse(body);
-      console.log(character.name);
-      if (index === count - 1) {
-        console.log('All characters printed.');
-      }
-    });
-  });
 });
 
+function printCharacters (characters, index) {
+  if (index >= characters.length) {
+    return;
+  }
+
+  request(characters[index], function (error, response, body) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      printCharacters(characters, index + 1);
+    }
+  });
+}
