032b5a7293a74a9c94a9d8c77cc68955a7cee66f
Andrew Rex Krakue <arkkrakue406@gmail.com>
2023-10-06 08:09:34 +0000

    Advanced tasks

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    function translate() {
+        const lang = $('#language_code').val();
+        $.get('https://www.fourtonfish.com/hellosalut/hello/' + lang, function(data) {
+            $('#hello').text(data.hello);
+        });
+    }
+    $('#btn_translate').click(translate);
+    $('#language_code').keypress(function(e) {
+        if (e.which == 13) {
+            translate();
+        }
+    });
+});
